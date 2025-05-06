97794f67abfcae8319d5b18544d44fabf8e5db71
NehaKulkarni28 <158441391+Nehak89@users.noreply.github.com>
2025-05-06 19:00:31 +0530

    Update numbers.scala

--- Group_B/numbers.scala
+++ Group_B/numbers.scala
@@ -14,3 +14,26 @@ val classifiedNumbers = inputfile .flatMap(line => line.split(" ")) .map(numStr 
 println(classifiedNumbers.toDebugString)
 classifiedNumbers.cache()
 classifiedNumbers.saveAsTextFile("output")
+
+
+//Method 2
+val inputfile = sc.textFile("/home/student/DSBDAL/numbers.txt")
+
+val classifiedNumbers = inputfile
+                        .flatMap(line => line.split(" ")) 
+                        .map(numStr => numStr.toInt)
+                        .map{num => 
+                              if (num > 0) 
+                                ("positive", 1)
+                              else if (num < 0) 
+                                ("negative", 1)
+                              else 
+                                ("zero", 1)
+    
+                         }
+                       .reduceByKey(_ + _);                    
+
+println(classifiedNumbers.toDebugString)
+classifiedNumbers.cache()
+classifiedNumbers.collect().foreach(println )//displays output on console
+classifiedNumbers.saveAsTextFile("output")
